15f0e64b260d164987d71205adbdfa9b1abc1bec
Vladislav Vaintroub <wlad@mariadb.com>
2016-06-17 21:42:08 +0000

    Added threadpool scheduler

---
+++ sql/threadpool_common.cc
@@ -0,0 +1,372 @@
+/* Copyright (C) 2012 Monty Program Ab
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#include <my_global.h>
+#include <violite.h>
+#include <sql_class.h>
+#include <sql_connect.h>
+#include <sql_audit.h>
+#include <debug_sync.h>
+#include <threadpool.h>
+#include <probes_mysql.h>
+#include <my_thread_local.h>
+#include <mysql/psi/mysql_idle.h>
+#include <conn_handler/channel_info.h>
+#include <conn_handler/connection_handler_manager.h>
+#include <conn_handler/connection_handler_impl.h>
+#include <mysqld_thd_manager.h>
+#include <mysql/thread_pool_priv.h>
+
+
+/* Threadpool parameters */
+
+uint threadpool_min_threads;
+uint threadpool_idle_timeout;
+uint threadpool_size;
+uint threadpool_max_size= 5000;
+uint threadpool_stall_limit;
+uint threadpool_max_threads;
+uint threadpool_oversubscribe;
+
+/* Stats */
+TP_STATISTICS tp_stats;
+
+
+extern bool do_command(THD*);
+
+/*
+  Worker threads contexts, and THD contexts.
+  =========================================
+  
+  Both worker threads and connections have their sets of thread local variables 
+  At the moment it is mysys_var (this has specific data for dbug, my_error and 
+  similar goodies), and PSI per-client structure.
+
+  Whenever query is executed following needs to be done:
+
+  1. Save worker thread context.
+  2. Change TLS variables to connection specific ones using thread_attach(THD*).
+     This function does some additional work , e.g setting up 
+     thread_stack/thread_ends_here pointers.
+  3. Process query
+  4. Restore worker thread context.
+
+  Connection login and termination follows similar schema w.r.t saving and 
+  restoring contexts. 
+
+  For both worker thread, and for the connection, mysys variables are created 
+  using my_thread_init() and freed with my_thread_end().
+
+*/
+#ifndef DBUG_OFF
+extern "C" thread_local_key_t THR_KEY_mysys;
+#endif
+
+struct Worker_thread_context
+{
+#ifdef HAVE_PSI_THREAD_INTERFACE
+  PSI_thread *psi_thread;
+#endif
+
+#ifndef DBUG_OFF
+  my_thread_id thread_id;
+#endif
+
+  void save()
+  {
+#ifdef HAVE_PSI_THREAD_INTERFACE
+    psi_thread= PSI_THREAD_CALL(get_thread)();
+#endif
+#ifndef DBUG_OFF
+    thread_id= my_thread_var_id();
+#endif
+  }
+
+  void restore()
+  {
+#ifdef HAVE_PSI_THREAD_INTERFACE
+    PSI_THREAD_CALL(set_thread)(psi_thread);
+#endif
+#ifndef DBUG_OFF
+    set_my_thread_var_id(thread_id);
+#endif
+    my_set_thread_local(THR_THD, 0);
+    my_set_thread_local(THR_MALLOC, 0);
+  }
+};
+
+
+/*
+Attach/associate the connection with the OS thread,
+*/
+static bool thread_attach(THD* thd)
+{
+#ifndef DBUG_OFF
+  set_my_thread_var_id(thd->thread_id());
+#endif
+  thd->thread_stack=(char*)&thd;
+  thd->store_globals();
+#ifdef HAVE_PSI_THREAD_INTERFACE
+  PSI_THREAD_CALL(set_thread)(thd->get_psi());
+#endif
+  mysql_socket_set_thread_owner(thd->get_protocol_classic()->get_vio()->mysql_socket);
+  return 0;
+}
+
+
+
+#ifdef HAVE_PSI_INTERFACE
+
+/*
+  The following fixes PSI "idle" psi instrumentation. The server assumes that connection
+  becomes idle just before net_read_packet() and switches to active after it.
+*/
+
+extern void net_before_header_psi(struct st_net *net, void *user_data, size_t /* unused: count */);
+
+static void dummy_net_before_header_psi(struct st_net *, void *, size_t)
+{
+}
+
+static void re_init_net_server_extension(THD *thd)
+{
+  NET *net= thd->get_protocol_classic()->get_net();
+  if (net->extension)
+  {
+    DBUG_ASSERT(net->extension == &thd->m_net_server_extension);
+    DBUG_ASSERT(thd->m_net_server_extension.m_before_header == net_before_header_psi);
+    thd->m_net_server_extension.m_before_header = dummy_net_before_header_psi;
+  }
+}
+
+#else
+
+#define re_init_net_server_extension(thd)
+
+#endif /* HAVE_PSI_INTERFACE */
+
+
+static inline void set_thd_idle(THD *thd)
+{
+  thd_set_net_read_write(thd, 1);
+  thd->m_server_idle = true;
+#ifdef HAVE_PSI_INTERFACE
+  NET* net= thd->get_protocol_classic()->get_net();
+  if (net->extension)
+  {
+    DBUG_ASSERT(net->extension == &thd->m_net_server_extension);
+    DBUG_ASSERT(thd->m_net_server_extension.m_before_header == dummy_net_before_header_psi);
+    net_before_header_psi(net, thd, 0);
+  }
+#endif
+}
+
+/* Abort a connection which does not have a corresponding THD */
+THD* threadpool_create_thd(Channel_info **channel_info)
+{
+  THD *thd = (*channel_info)->create_thd();
+  if (!thd)
+  {
+    (*channel_info)->send_error_and_close_channel(ER_OUT_OF_RESOURCES, 0, false);
+    Connection_handler_manager::get_instance()->inc_aborted_connects();
+    Connection_handler_manager::dec_connection_count();
+  }
+  delete *channel_info;
+  *channel_info= 0;
+  return thd;
+}
+
+
+int threadpool_add_connection(THD *thd)
+{
+  int retval=1;
+
+  Worker_thread_context worker_context;
+  worker_context.save();
+  /*
+    Create a new connection context: mysys_thread_var and PSI thread
+    Store them in THD.
+  */
+  //my_thread_init();
+
+  thd->set_new_thread_id();
+
+  /* Create new PSI thread for use with the THD. */
+#ifdef HAVE_PSI_THREAD_INTERFACE
+  thd->set_psi(PSI_THREAD_CALL(new_thread)(key_thread_one_connection, thd,
+    thd->thread_id()));
+#endif
+
+  /* Login. */
+  thread_attach(thd);
+  re_init_net_server_extension(thd);
+  ulonglong now= my_micro_time();
+  thd->start_utime= now;
+  thd->thr_create_utime= now;
+
+  if (thd->store_globals())
+  {
+    close_connection(thd, ER_OUT_OF_RESOURCES);
+    goto end;
+  }
+
+  Global_THD_manager *thd_manager= Global_THD_manager::get_instance();
+  thd_manager->add_thd(thd);
+
+  if (thd_prepare_connection(thd))
+    goto end;
+
+   /* 
+    Check if THD is ok, as prepare_new_connection_state()
+    can fail, for example if init command failed.
+   */
+  if (!thd_connection_alive(thd))
+    goto end;
+
+  retval= 0;
+  set_thd_idle(thd);
+
+end:
+  if (retval)
+  {
+    Connection_handler_manager::get_instance()->inc_aborted_connects();
+  }
+  worker_context.restore();
+  return retval;
+}
+
+
+void threadpool_remove_connection(THD *thd)
+{
+
+  Worker_thread_context worker_context;
+  worker_context.save();
+
+  thread_attach(thd);
+  thd_set_net_read_write(thd, 0);
+
+  end_connection(thd);
+  close_connection(thd, 0);
+
+  thd->get_stmt_da()->reset_diagnostics_area();
+  thd->release_resources();
+  Global_THD_manager::get_instance()->remove_thd(thd);
+  dec_connection_count();
+
+  delete thd;
+
+  worker_context.restore();
+}
+
+/**
+ Process a single client request or a single batch.
+*/
+int threadpool_process_request(THD *thd)
+{
+  int retval= 0;
+  Worker_thread_context  worker_context;
+  worker_context.save();
+
+  thread_attach(thd);
+  thd_set_net_read_write(thd, 0);
+
+  if (thd->killed == THD::KILL_CONNECTION)
+  {
+    /* 
+      killed flag was set by timeout handler 
+      or KILL command. Return error.
+    */
+    retval= 1;
+    goto end;
+  }
+
+
+  /*
+    In the loop below, the flow is essentially the copy of thead-per-connections
+    logic, see do_handle_one_connection() in sql_connect.c
+
+    The goal is to execute a single query, thus the loop is normally executed 
+    only once. However for SSL connections, it can be executed multiple times 
+    (SSL can preread and cache incoming data, and vio->has_data() checks if it 
+    was the case).
+  */
+  for(;;)
+  {
+    Vio *vio;
+    thd_set_net_read_write(thd, 0);
+    mysql_audit_release(thd);
+
+    if ((retval= do_command(thd)) != 0)
+      goto end;
+    if (!thd_connection_alive(thd))
+    {
+      retval= 1;
+      goto end;
+    }
+
+    set_thd_idle(thd);
+
+    vio= thd->get_protocol_classic()->get_net()->vio;
+    if (!vio->has_data(vio))
+    { 
+      /* More info on this debug sync is in sql_parse.cc*/
+      DEBUG_SYNC(thd, "before_do_command_net_read");
+      goto end;
+    }
+  }
+
+end:
+  worker_context.restore();
+  return retval;
+}
+
+void tp_post_kill_notification(THD *thd)
+{
+  if (thd == current_thd || thd->system_thread)
+    return;
+  Vio *vio= thd->get_protocol_classic()->get_net()->vio;
+  if (vio)
+    vio_shutdown(vio,  SHUT_RD);
+}
+
+THD_event_functions tp_event_functions=
+{
+  tp_wait_begin, tp_wait_end,0
+};
+
+
+/* Connection handler class*/
+Thread_pool_connection_handler::Thread_pool_connection_handler()
+{
+  if (tp_init())
+    abort();
+  Connection_handler_manager::event_functions= &tp_event_functions;
+}
+
+Thread_pool_connection_handler::~Thread_pool_connection_handler()
+{
+  tp_end();
+}
+
+bool Thread_pool_connection_handler::add_connection(Channel_info *channel_info)
+{
+  return tp_add_connection(channel_info);
+}
+
+uint Thread_pool_connection_handler::get_max_threads() const
+{
+  return threadpool_max_threads;
+}
+
---
+++ sql/threadpool_win.cc
@@ -0,0 +1,605 @@
+/* Copyright (C) 2012 Monty Program Ab
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#ifdef _WIN32_WINNT
+#undef _WIN32_WINNT
+#endif
+
+#define _WIN32_WINNT 0x0601
+
+#include <my_global.h>
+#include <violite.h>
+#include <mysql/thread_pool_priv.h>
+#include <sql_class.h>
+#include <sql_connect.h>
+#include <mysqld.h>
+#include <debug_sync.h>
+#include <threadpool.h>
+#include <windows.h>
+#include <conn_handler/channel_info.h>
+
+#ifndef ULONGLONG_MAX
+#define ULONGLONG_MAX _UI64_MAX
+#endif
+
+
+/* Log a warning */
+static void tp_log_warning(const char *msg, const char *fct)
+{
+  sql_print_warning("Threadpool: %s. %s failed (last error %d)",msg, fct,
+    GetLastError());
+}
+
+
+PTP_POOL pool;
+DWORD fls;
+
+static bool skip_completion_port_on_success = false;
+
+/*
+  Threadpool callbacks.
+
+  io_completion_callback  - handle client request
+  timer_callback - handle wait timeout (kill connection)
+  shm_read_callback, shm_close_callback - shared memory stuff
+  login_callback - user login (submitted as threadpool work)
+
+*/
+
+static void CALLBACK timer_callback(PTP_CALLBACK_INSTANCE instance, 
+  PVOID context, PTP_TIMER timer);
+
+static void CALLBACK io_completion_callback(PTP_CALLBACK_INSTANCE instance, 
+  PVOID context,  PVOID overlapped,  ULONG io_result, ULONG_PTR nbytes, PTP_IO io);
+
+static void CALLBACK shm_read_callback(PTP_CALLBACK_INSTANCE instance,
+  PVOID Context, PTP_WAIT wait,TP_WAIT_RESULT wait_result);
+
+static void CALLBACK shm_close_callback(PTP_CALLBACK_INSTANCE instance,
+  PVOID Context, PTP_WAIT wait,TP_WAIT_RESULT wait_result);
+
+static void check_thread_init();
+
+/* Get current time as Windows time */
+static ulonglong now()
+{
+  ulonglong current_time;
+  GetSystemTimeAsFileTime((PFILETIME)&current_time);
+  return current_time;
+}
+
+/* 
+  Connection structure, encapsulates THD + structures for asynchronous
+  IO and pool.
+*/
+
+struct connection_t
+{
+  THD *thd;
+  Channel_info *channel_info;
+  HANDLE handle;
+  OVERLAPPED overlapped;
+  /* absolute time for wait timeout (as Windows time) */
+  volatile ulonglong timeout; 
+  TP_CALLBACK_ENVIRON callback_environ;
+  PTP_IO  io;
+  PTP_TIMER timer;
+  PTP_WAIT shm_read;
+  /* Callback instance, used to inform treadpool about long callbacks */
+  PTP_CALLBACK_INSTANCE callback_instance;
+  bool logged_in;
+
+  Vio *get_vio()
+  {
+    return thd->get_protocol_classic()->get_net()->vio;
+  }
+};
+
+
+void init_connection(connection_t *connection)
+{
+  connection->logged_in = false;
+  connection->handle= 0;
+  connection->io= 0;
+  connection->shm_read= 0;
+  connection->timer= 0;
+  connection->logged_in = false;
+  connection->timeout=ULONGLONG_MAX;
+  connection->callback_instance= 0;
+  memset(&connection->overlapped, 0, sizeof(OVERLAPPED));
+  InitializeThreadpoolEnvironment(&connection->callback_environ);
+  SetThreadpoolCallbackPool(&connection->callback_environ, pool);
+  connection->thd = 0;
+}
+
+
+int init_io(connection_t *connection, THD *thd)
+{
+  connection->thd= thd;
+  Vio *vio = thd->get_protocol_classic()->get_net()->vio;
+
+  switch(vio->type)
+  {
+    case VIO_TYPE_SSL:
+    case VIO_TYPE_TCPIP:
+      connection->handle = (HANDLE)vio->mysql_socket.fd;
+      break;
+    case VIO_TYPE_NAMEDPIPE:
+      connection->handle= (HANDLE)vio->hPipe;
+      break;
+    case VIO_TYPE_SHARED_MEMORY:
+      connection->shm_read=  CreateThreadpoolWait(shm_read_callback, connection, 
+        &connection->callback_environ);
+      if (!connection->shm_read)
+      {
+        tp_log_warning("Allocation failed", "CreateThreadpoolWait");
+        return -1;
+      }
+      break;
+    default:
+      abort();
+  }
+
+  if (connection->handle)
+  {
+    /* Performance tweaks (s. MSDN documentation)*/
+    UCHAR flags= FILE_SKIP_SET_EVENT_ON_HANDLE;
+    if (skip_completion_port_on_success)
+    {
+      flags |= FILE_SKIP_COMPLETION_PORT_ON_SUCCESS;
+    }
+    (void)SetFileCompletionNotificationModes(connection->handle, flags);
+
+    /* Assign io completion callback */
+    connection->io= CreateThreadpoolIo(connection->handle, 
+      io_completion_callback, connection, &connection->callback_environ);
+    if(!connection->io)
+    {
+      tp_log_warning("Allocation failed", "CreateThreadpoolWait");
+      return -1;
+    }
+  }
+  connection->timer= CreateThreadpoolTimer(timer_callback, connection, 
+    &connection->callback_environ);
+  if (!connection->timer)
+  {
+    tp_log_warning("Allocation failed", "CreateThreadpoolWait");
+    return -1;
+  }
+
+  return 0;
+}
+
+
+/*
+  Start asynchronous read
+*/
+int start_io(connection_t *connection, PTP_CALLBACK_INSTANCE instance)
+{
+  /* Start async read */
+  DWORD num_bytes = 0;
+  static char c;
+  WSABUF buf;
+  buf.buf= &c;
+  buf.len= 0;
+  DWORD flags=0;
+  DWORD last_error= 0;
+
+  int retval;
+  Vio *vio= connection->get_vio();
+
+  if (vio->type == VIO_TYPE_SHARED_MEMORY)
+  {
+      SetThreadpoolWait(connection->shm_read, vio->event_server_wrote, NULL);
+      return 0;
+  }
+  
+  DBUG_ASSERT(vio->type == VIO_TYPE_TCPIP || 
+    vio->type == VIO_TYPE_SSL ||
+    vio->type == VIO_TYPE_NAMEDPIPE);
+
+  OVERLAPPED *overlapped= &connection->overlapped;
+  PTP_IO io= connection->io;
+  StartThreadpoolIo(io);
+
+  if (vio->type == VIO_TYPE_TCPIP || vio->type == VIO_TYPE_SSL)
+  {
+    /* Start async io (sockets). */
+    if (WSARecv(vio->mysql_socket.fd , &buf, 1, &num_bytes, &flags,
+          overlapped,  NULL) == 0)
+    {
+        retval= last_error= 0;
+    }
+    else
+    {
+      retval= -1;
+      last_error=  WSAGetLastError();
+    }
+  }
+  else
+  {
+    /* Start async io (named pipe) */
+    if (ReadFile(vio->hPipe, &c, 0, &num_bytes ,overlapped))
+    {
+      retval= last_error= 0;
+    }
+    else
+    {
+      retval= -1;
+      last_error= GetLastError();
+    }
+  }
+
+  if (retval == 0 || last_error == ERROR_MORE_DATA)
+  {
+    /*
+      IO successfully finished (synchronously). 
+      If skip_completion_port_on_success is set, we need to handle it right 
+      here, because completion callback would not be executed by the pool.
+    */
+    if(skip_completion_port_on_success)
+    {
+      CancelThreadpoolIo(io);
+      io_completion_callback(instance, connection, overlapped, last_error, 
+        num_bytes, io);
+    }
+    return 0;
+  }
+
+  if(last_error == ERROR_IO_PENDING)
+  {
+    return 0;
+  }
+
+  /* Some error occured */
+  CancelThreadpoolIo(io);
+  return -1;
+}
+
+
+int login(connection_t *connection, PTP_CALLBACK_INSTANCE instance)
+{
+  if (threadpool_add_connection(connection->thd) == 0
+      && init_io(connection, connection->thd) == 0
+      && start_io(connection, instance) == 0)
+  {
+    return 0;
+  }
+  return -1;
+}
+
+/*
+  Recalculate wait timeout, maybe reset timer. 
+*/
+void set_wait_timeout(connection_t *connection, ulonglong old_timeout)
+{
+  ulonglong new_timeout = now() + 
+    10000000LL*connection->thd->variables.net_wait_timeout;
+
+  if (new_timeout < old_timeout)
+  {
+    SetThreadpoolTimer(connection->timer, (PFILETIME) &new_timeout, 0, 1000);
+  }
+  connection->timeout = new_timeout;
+}
+
+
+/* Connection destructor */
+void destroy_connection(connection_t *connection, PTP_CALLBACK_INSTANCE instance)
+{
+  if (instance)
+    DisassociateCurrentThreadFromCallback(instance);
+  if (connection->io)
+  {
+     WaitForThreadpoolIoCallbacks(connection->io, TRUE); 
+     CloseThreadpoolIo(connection->io);
+  }
+
+  if(connection->shm_read)
+  {
+    WaitForThreadpoolWaitCallbacks(connection->shm_read, TRUE);
+    CloseThreadpoolWait(connection->shm_read);
+  }
+
+  if(connection->timer)
+  {
+    SetThreadpoolTimer(connection->timer, 0, 0, 0);
+    WaitForThreadpoolTimerCallbacks(connection->timer, TRUE);
+    CloseThreadpoolTimer(connection->timer);
+  }
+  
+  if (connection->thd)
+  {
+    threadpool_remove_connection(connection->thd);
+  }
+ 
+
+  DestroyThreadpoolEnvironment(&connection->callback_environ);
+}
+
+
+
+/* 
+  This function should be called first whenever a callback is invoked in the 
+  threadpool, does my_thread_init() if not yet done
+*/
+static void check_thread_init()
+{
+  if (FlsGetValue(fls) == NULL)
+  {
+    my_thread_init();
+    FlsSetValue(fls, (void *)1);
+    inc_thread_created();
+    InterlockedIncrement((volatile long *)&tp_stats.num_worker_threads);
+  }
+}
+
+
+/*
+  Decrement number of threads when a thread exits . 
+  On Windows, FlsAlloc() provides the thread destruction callbacks.
+*/
+static VOID WINAPI thread_destructor(void *data)
+{
+  if(data)
+  {
+    my_thread_end();
+    InterlockedDecrement((volatile long *)&tp_stats.num_worker_threads);
+  }
+}
+
+
+/* Scheduler callback : init */
+bool tp_init(void)
+{
+  fls= FlsAlloc(thread_destructor);
+  pool= CreateThreadpool(NULL);
+  if(!pool)
+  {
+    sql_print_error("Can't create threadpool. "
+      "CreateThreadpool() failed with %d. Likely cause is memory pressure", 
+      GetLastError());
+    exit(1);
+  }
+
+  if (threadpool_max_threads)
+  {
+    SetThreadpoolThreadMaximum(pool,threadpool_max_threads);
+  }
+
+  if (threadpool_min_threads)
+  {
+    if (!SetThreadpoolThreadMinimum(pool, threadpool_min_threads))
+    {
+      tp_log_warning( "Can't set threadpool minimum threads", 
+        "SetThreadpoolThreadMinimum");
+    }
+  }
+
+  TP_POOL_STACK_INFORMATION stackinfo;
+  stackinfo.StackCommit = 0;
+  stackinfo.StackReserve = (SIZE_T)my_thread_stack_size;
+  if (!SetThreadpoolStackInformation(pool, &stackinfo))
+  {
+    tp_log_warning("Can't set threadpool stack size", 
+      "SetThreadpoolStackInformation");
+  }
+
+  return 0;
+}
+
+
+/**
+  Scheduler callback : Destroy the scheduler.
+*/
+void tp_end(void)
+{
+  if(pool)
+  {
+    SetThreadpoolThreadMaximum(pool, 0);
+    CloseThreadpool(pool);
+  }
+}
+
+
+/*
+  Handle read completion/notification.
+*/
+static VOID CALLBACK io_completion_callback(PTP_CALLBACK_INSTANCE instance, 
+  PVOID context,  PVOID overlapped,  ULONG io_result, ULONG_PTR nbytes, PTP_IO io)
+{
+  if(instance)
+  {
+    check_thread_init();
+  }
+
+  connection_t *connection = (connection_t*)context;
+
+  if (io_result != ERROR_SUCCESS)
+    goto error;
+
+  THD *thd= connection->thd;
+  ulonglong old_timeout = connection->timeout;
+  connection->timeout = ULONGLONG_MAX;
+  connection->callback_instance= instance;
+  if (threadpool_process_request(connection->thd))
+    goto error;
+
+  set_wait_timeout(connection, old_timeout);
+  if(start_io(connection, instance))
+    goto error;
+
+  return;
+
+error:
+  /* Some error has occured. */
+
+  destroy_connection(connection, instance);
+  free(connection);
+}
+
+
+/* Simple callback for login */
+static void CALLBACK login_callback(PTP_CALLBACK_INSTANCE instance, 
+  PVOID context, PTP_WORK work)
+{
+ 
+  if(instance)
+  {
+    check_thread_init();
+  }
+  connection_t *connection= (connection_t *)context;
+  connection->thd= threadpool_create_thd(&connection->channel_info);
+  if (!connection->thd || login(connection, instance))
+  {
+    destroy_connection(connection, instance);
+    free(connection);
+  }
+}
+
+/*
+  Timer callback.
+  Invoked when connection times out (wait_timeout)
+*/
+static VOID CALLBACK timer_callback(PTP_CALLBACK_INSTANCE instance, 
+  PVOID parameter, PTP_TIMER timer)
+{
+  check_thread_init();
+
+  connection_t *con= (connection_t*)parameter;
+  ulonglong timeout= con->timeout;
+
+  if (timeout <= now())
+  {
+    con->thd->killed = THD::KILL_CONNECTION;
+    if (con->thd)
+	     tp_post_kill_notification(con->thd);
+  }
+  else if(timeout != ULONGLONG_MAX)
+  {
+    /* 
+      Reset timer. 
+      There is a tiny possibility of a race condition, since the value of timeout 
+      could have changed to smaller value in the thread doing io callback. 
+
+      Given the relative unimportance of the wait timeout, we accept race 
+      condition.
+    */
+    SetThreadpoolTimer(timer, (PFILETIME)&timeout, 0, 1000);
+  }
+}
+
+
+/*
+  Shared memory read callback.
+  Invoked when read event is set on connection.
+*/
+static void CALLBACK shm_read_callback(PTP_CALLBACK_INSTANCE instance,
+  PVOID context, PTP_WAIT wait,TP_WAIT_RESULT wait_result)
+{
+  connection_t *con= (connection_t *)context;
+  /* Disarm wait. */
+  SetThreadpoolWait(wait, NULL, NULL);
+
+  /* 
+    This is an autoreset event, and one wakeup is eaten already by threadpool,
+    and the current state is "not set". Thus we need to reset the event again, 
+    or vio_read will hang.
+  */
+  HANDLE h = con->get_vio()->event_server_wrote;
+  SetEvent(h);
+  io_completion_callback(instance, context, NULL, 0, 0 , 0);
+}
+
+
+/*
+  Notify the thread pool about a new connection.
+*/
+bool tp_add_connection(Channel_info *ci)
+{
+  connection_t *connection= (connection_t *)malloc(sizeof(connection_t));
+  if (!connection)
+    return true;
+
+  init_connection(connection);
+  connection->channel_info= ci;
+
+  PTP_WORK wrk =  CreateThreadpoolWork(login_callback,connection, NULL);
+  if (!wrk)
+  {
+    free(connection);
+    return true;
+  }
+  SubmitThreadpoolWork(wrk);
+  CloseThreadpoolWork(wrk);
+  return false;
+}
+
+
+/**
+  Sets the number of idle threads the thread pool maintains in anticipation of new
+  requests.
+*/
+void tp_set_min_threads(uint val)
+{
+  if (pool)
+    SetThreadpoolThreadMinimum(pool, val);
+}
+
+void tp_set_max_threads(uint val)
+{
+  if (pool)
+    SetThreadpoolThreadMaximum(pool, val);
+}
+
+void tp_wait_begin(THD *thd, int type)
+{
+  /*
+    Signal to the threadpool whenever callback can run long. Currently, binlog
+    waits are a good candidate, its waits are really long
+  */
+  if (type == THD_WAIT_BINLOG)
+  {
+	if (!thd)
+	  thd = _current_thd();
+    connection_t *connection= (connection_t *)thd->scheduler.data;
+    if(connection && connection->callback_instance)
+    {
+      CallbackMayRunLong(connection->callback_instance);
+      /* 
+        Reset instance, to avoid calling CallbackMayRunLong  twice within 
+        the same callback (it is an error according to docs).
+      */
+      connection->callback_instance= 0;
+    }
+  }
+}
+
+void tp_wait_end(THD *thd) 
+{
+  /* Do we need to do anything ? */
+}
+
+
+/**
+ Number of idle threads in pool.
+ This info is not available in Windows implementation,
+ thus function always returns 0.
+*/
+int tp_get_idle_thread_count()
+{
+  return 0;
+}
+
